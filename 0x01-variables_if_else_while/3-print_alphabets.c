b3a8e894d75db601667b6047fcc4c8ba8fa9bf15
Peter Maina <petermaina546@gmail.com>
2023-06-17 08:54:13 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  * main - Starting of the program
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+int lowerCase = 'a';
+int upperCase = 'A';
+
+while (lowerCase <= 'z')
+{
+putchar(lowerCase);
+lowerCase += 1;
+}
+while (upperCase <= 'z')
+{
+putchar(upperCase);
+upperCase += 1;
+}
+putchar('\n');
+return (0);
+}
